47960f775bd5469ff2793284ae90a78e8666ff97
Valentin <vb@talance.io>
2020-10-13 13:07:34 +0300

    Add User folder with module

---
+++ front-end/store-frontend/src/app/features/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { UserRoutingModule } from './user-routing.module';
+
+
+const declarations = [
+];
+
+@NgModule({
+  declarations: [...declarations],
+  entryComponents: [
+  
+  ],
+  imports: [
+    UserRoutingModule
+  ],
+  providers: [],
+})
+export class UserModule {}
